ca839624791ab72535e6919c278bc9940b6e958f
Dejan Dmitrović <dmitrovyu@users.noreply.github.com>
2024-09-09 23:28:47 +0200

    Algebra: solve quadratic equations

--- Chapter_03/Exercise_03_01/Exercise_03_01.java
+++ Chapter_03/Exercise_03_01/Exercise_03_01.java
@@ -9,10 +9,12 @@ import java.util.Scanner;
  *  b2 - 4ac is called the discriminant of the quadratic equation. If it is positive, the
  * equation has two real roots. If it is zero, the equation has one root. If it is negative,
  * the equation has no real roots.
+ 
  * Write a program that prompts the user to enter values for a, b, and c and displays
  * the result based on the discriminant. If the discriminant is positive, display two
  * roots. If the discriminant is 0, display one root. Otherwise, display “The equation
  * has no real roots.”
+ 
  * Note you can use Math.pow(x, 0.5) to compute 2x. Here are some sample
  * runs:
  *          Enter a, b, c: 1.0 3 1
